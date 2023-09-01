7c337c1cf6b2ca3fc1da14fe6ea9b825600a3d4f
tanvir1017 <developer.tanvirhossain@gmail.com>
2023-09-01 12:38:44 +0600

    feat: 🎸 transorming the old version to version1

---
+++ pages/_app.jsx
@@ -0,0 +1,39 @@
+import { MDXProvider } from "@mdx-js/react";
+import { AnimatePresence } from "framer-motion";
+import { ThemeProvider } from "next-themes";
+
+import NextNProgress from "nextjs-progressbar";
+import Footer from "../components/shared/footer/footer";
+import MobileNav from "../components/shared/nav/Navigation/MobileNav";
+
+import NavBar from "components/shared/navbar/nav";
+import Toptop from "components/shared/toptop/Toptop";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps, router }) {
+  const { pathname } = router;
+
+  return (
+    <ThemeProvider
+      forcedTheme={Component.theme || null}
+      enableSystem={true}
+      attribute="class"
+    >
+      <MDXProvider>
+        <main>
+          <NextNProgress color="#857a7a" />
+          {/* {pathname !== "/404" && <Toptop />} */}
+          {pathname !== "/404" && <Toptop />}
+          <MobileNav />
+          {pathname !== "/404" && <NavBar />}
+          <AnimatePresence>
+            <Component key={router.pathname} {...pageProps} />
+          </AnimatePresence>
+          {pathname !== "/404" && <Footer />}
+        </main>
+      </MDXProvider>
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
